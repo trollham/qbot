bebf07baff2fcdde0e53677da1f2c3565b257f7d
Eric Huff <thegreaterici@gmail.com>
2021-04-04 11:19:43 -0600

    Split Queue into QueueControls and QueueEntry
    QueueControls was created to encompass the controls exposed to the queue
    admin, making it easier to hide from unauthorized access when user scopes
    are implemented.

---
+++ frontend/src/components/QueueControls.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar navbar-dark">
+    <button v-on:click="next">Next</button>
+    <button v-on:click="toggle_open" v-if="is_open">Close</button>
+    <button v-on:click="toggle_open" v-else>Open</button>
+    <input
+      class="form-control form-control-dark"
+      v-model="pop_size"
+      placeholder="Party size"
+    />
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "QueueControls"
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/QueueEntry.vue
@@ -0,0 +1,31 @@
+<template>
+  <tr>
+    <th scope="row">{{ index }}</th>
+    <td>{{ entry.nickname }}</td>
+    <td>{{ entry.time_joined }}</td>
+    <td>
+      <button @click="remove(user)" class="btn btn-outline-danger">
+        <font-awesome-icon icon="minus-circle" />
+        Remove
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    entry: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
